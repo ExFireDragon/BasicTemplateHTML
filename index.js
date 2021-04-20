fceca8b4ae2a5cb6e6f8e8a994abb00ed377e956
Lucas Monzón <lucasreddit@gmail.com>
2021-04-20 18:35:22 -0300

    Cambios para que el código esté listo para el host.

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const http = require('http');
+const app = express();
+const path = require('path');
+
+app.use(express.json());
+express.static("static");
+
+app.use('/', (req, res) => {
+  res.sendFile(path.join(__dirname +'/static/index.html'));
+});
+
+const server = http.createServer(app);
+const port = 3000;
+server.listen(port);
+console.debug('Server listening on port ' + port);
